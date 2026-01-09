636d4425a073c8a3d92dd0face54ef5af863be28
Instant-SkyTechnology <stevekalala@yahoo.fr>
2026-01-09 16:04:57 -0500

    Fixed some bugs and added social media

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,10 @@
 
 const navButton = document.getElementById('nav-button');
-const nav = document.getElementById('main-nav'); // target navigation partial
+const nav = document.getElementById('main-nav');
 
 navButton.addEventListener('click', () => {
   nav.classList.toggle('active');   // show/hide nav on mobile
   navButton.classList.toggle('show'); // change hamburger to X
 });
+
 
--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(staticRoutes)
 
+
 /* **************************
  * View Engine and Templates
  ****************************/
