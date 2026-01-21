68e6a6b8f31bd9be6b828b6d3ad546eddf95690c
Instant-SkyTechnology <stevekalala@yahoo.fr>
2026-01-21 16:16:01 -0500

    Fixed some bugs

--- controllers/invController.js
+++ controllers/invController.js
@@ -6,22 +6,50 @@ const invCont = {}
 /* ****************************************
  *  Build inventory by classification view
  * ****************************************/
+// invCont.buildByClassificationId = async function (req, res, next) {
+//     const classification_id = req.params.classificationId
+//     const data = await invModel.getInventoryByClassificationId(classification_id)
+//     // console.log("Vehicle data:", data)
+//     // console.log("Vehicle thumbnails:", data.map(v => v.inv_thumbnail)) /*  These two lines help to debug the thumbnail*/
+
+
+//   const grid = await utilities.buildClassificationGrid(data)
+//   let nav = await utilities.getNav()
+//   const className = data[0].classification_name
+//   res.render("./inventory/classification", {
+//     title: className + " vehicles",
+//     nav,
+//     grid,
+//   })
+// }
 invCont.buildByClassificationId = async function (req, res, next) {
+  try {
     const classification_id = req.params.classificationId
     const data = await invModel.getInventoryByClassificationId(classification_id)
-    // console.log("Vehicle data:", data)
-    // console.log("Vehicle thumbnails:", data.map(v => v.inv_thumbnail)) /*  These two lines help to debug the thumbnail*/
+    const nav = await utilities.getNav()
 
+    // ✅ Prevent crash if no vehicles exist
+    if (!data || data.length === 0) {
+      return res.render("./inventory/classification", {
+        title: "No Vehicles Found",
+        nav,
+        grid: '<p class="notice">Sorry, no matching vehicles could be found.</p>'
+      })
+    }
 
-  const grid = await utilities.buildClassificationGrid(data)
-  let nav = await utilities.getNav()
-  const className = data[0].classification_name
-  res.render("./inventory/classification", {
-    title: className + " vehicles",
-    nav,
-    grid,
-  })
+    const grid = await utilities.buildClassificationGrid(data)
+    const className = data[0].classification_name
+
+    res.render("./inventory/classification", {
+      title: `${className} vehicles`,
+      nav,
+      grid,
+    })
+  } catch (error) {
+    next(error)
+  }
 }
+
 
 /* ****************************
  *  Build vehicle detail view
