f759659c2a005b4f2f5210566287a1b3c6c30349
Instant-SkyTechnology <stevekalala@yahoo.fr>
2026-01-21 13:51:56 -0500

    Fixed something

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use("/inv", inventoryRoute)
 
 // 404 handler
 app.use((req, res, next) => {
-  const error = new Error("Page Not Found")
+  const error = new Error("Sorry, we appear to have lost that page.")
   error.status = 404
   next(error)
 })
